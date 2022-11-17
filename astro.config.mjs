f945bd16482d7eb52511aca4abc20e8e80bdd6b6
Hansanghyeon <999@hyeon.pro>
2022-11-17 09:21:07 +0000

    fix: a typo astrojs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 
 import sitemap from '@astrojs/sitemap'
-import react from '@astro/react'
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
