65fdde5d094fd4b6aa06c5ca3226bae5d2bd9865
Hansanghyeon <999@hyeon.pro>
2022-11-20 04:03:18 +0000

    wip: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
-const Header = () => {
+interface HeaderProps {
+  siteTitle: string
+}
+const Header = ({ siteTitle }: HeaderProps) => {
   return (
-    <div>Header</div>
+    <div className='w-[240px]'>
+      <div className='flex justify-center'>
+        <div className='text-[24px] font-RobotoSlab font-semibold'>{siteTitle}<span className="text-green-500">.</span></div>
+      </div>
+      <div className='mt-[60px]'>
+        <div className='grid justify-items-center'>
+          <svg width="100px" height="100px" xmlns="http://www.w3.org/2000/svg">
+            <defs>
+              <clipPath id="circle">
+                <circle cx='50' cy='50' r='40' />
+              </clipPath>
+            </defs>
+            <image href="https://source.unsplash.com/random/80x80" x={`10`} y={`10`} width={`80`} height={`80`} clipPath="url(#circle)" />
+          </svg>
+          <span>Jerome Wilson</span>
+          <div></div>
+        </div>
+      </div>
+      <div>menu</div>
+      <div>footer</div>
+    </div>
   )
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        RobotoSlab: ['"Roboto Slab", serif'],
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
